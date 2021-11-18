6f57f1cbf5aa0760628882c1ed970744c13cfeed
thisanches07 <thi.sanches@hotmail.com>
2021-11-18 17:14:48 -0300

    adicionando tela de fim

--- client/tetris.js
+++ client/tetris.js
@@ -94,6 +94,11 @@ class Tetris{ //Construção do tetris (o conjunto inteiro de arena, jogador e o
 
     updateScore(score) { //Atualiza contador de pontos
         this.element.querySelector('.score').innerText = score;
+        if(score ==10 )
+        {
+            window.location.href = './end.html';
+        }
     }
+    
 }
 
